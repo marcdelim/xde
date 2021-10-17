6fc7d127d7833f3dee0da88629121fafd7809e56
Marc Delim <marcjosephdelim@gmail.com>
2021-10-17 20:08:32 +0800

    fix design

--- application/modules/dashboard/views/index.php
+++ application/modules/dashboard/views/index.php
@@ -50,7 +50,7 @@
                                                 <div class="chartjs-size-monitor-expand" style="position:absolute;left:0;top:0;right:0;bottom:0;overflow:hidden;pointer-events:none;visibility:hidden;z-index:-1;">
                                                     <div style="position:absolute;width:1000000px;height:1000000px;left:0;top:0"></div>
                                                 </div>
-                                                <div class="chartjs-size-monitor-shrink" style="position:absolute;left:0;top:0;right:0;bottom:0;overflow:hidden;pointer-events:none;visibility:hidden;z-index:-1;">
+                                                <div class="chartjs-size-monitor-shrink" sty    le="position:absolute;left:0;top:0;right:0;bottom:0;overflow:hidden;pointer-events:none;visibility:hidden;z-index:-1;">
                                                     <div style="position:absolute;width:200%;height:200%;left:0; top:0"></div>
                                                 </div>
                                             </div> 
